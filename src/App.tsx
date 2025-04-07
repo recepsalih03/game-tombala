0840b8d9c252e1834345d2a5663f4c239e609780
Recep Salih Kılınç <recepsalihkilinc@hotmail.com>
2025-04-07 16:16:44 +0300

    Proje kurulumları tamamlandı.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TombalaGame from './components/TombalaGame';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Tombala App</h1>
+      <TombalaGame />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TombalaGame.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TombalaGame: React.FC = () => {
+  // Basit bir placeholder
+  return (
+    <div>
+      <h2>Tombala Game</h2>
+      <p>Burada tombala kartları, çekilen numaralar vs. görünecek</p>
+    </div>
+  );
+};
+
+export default TombalaGame;
